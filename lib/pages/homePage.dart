9fa936932786947aa819dbd77590e6b07dd6bdd6
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-06 10:43:20 +0530

    Home page in progress

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -10,8 +10,28 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: AIColors.pimaryColor1,
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children: <Widget>[
+          VxAnimatedBox()
+              .size(context.screenWidth, context.screenHeight)
+              .withGradient(LinearGradient(colors: [
+                AIColors.pimaryColor1,
+                AIColors.pimaryColor2,
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight))
+              .make(),
+          AppBar(
+            title: "AI RADIO".text.xl4.bold.white.make().shimmer(
+              primaryColor: Vx.purple300,
+              secondaryColor: Colors.white
+            ),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            centerTitle: true,
+          ).h(100).p16(),
+        ],
+      ),
     );
   }
 }
